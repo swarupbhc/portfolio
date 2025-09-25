956ad87f12293a3bbcf84fd1748d00fec40b76f6
Swarup <55395995+swarupbhc@users.noreply.github.com>
2025-09-25 14:06:35 +0530

    init

--- src/data/resume.tsx
+++ src/data/resume.tsx
@@ -2,17 +2,19 @@ import { Icons } from "@/components/icons";
 import { HomeIcon, NotebookIcon } from "lucide-react";
 
 export const DATA = {
-  name: "Dillion Verma",
-  initials: "DV",
-  url: "https://dillion.io",
-  location: "San Francisco, CA",
-  locationLink: "https://www.google.com/maps/place/sanfrancisco",
+  name: "Swarup Bhanja Chowdhury",
+  initials: "SWBC",
+  url: "https://swarup.dev",
+  location: "Kolkata, India",
+  locationLink: "https://maps.app.goo.gl/Rk6w5sZuvpkhqmc98",
   description:
-    "Software Engineer turned Entrepreneur. I love building things and helping people. Very active on Twitter.",
+    "Software Engineer, Cross platform mobile application developer. Also experienced in React and Express JS",
   summary:
-    "At the end of 2022, I quit my job as a software engineer to go fulltime into building and scaling my own SaaS businesses. In the past, [I pursued a double degree in computer science and business](/#education), [interned at big tech companies in Silicon Valley](https://www.youtube.com/watch?v=d-LJ2e5qKdE), and [competed in over 21 hackathons for fun](/#hackathons). I also had the pleasure of being a part of the first ever in-person cohort of buildspace called [buildspace sf1](https://buildspace.so/sf1).",
+    "Passionate Mobile & Backend Developer, [I pursued a double degree in computer application and history](/#education), [Located in Kolkata](https://maps.app.goo.gl/Rk6w5sZuvpkhqmc98), Expert in crafting dynamic mobile experiences with Jetpack Compose for modern Android UI development. Skilled in building high-performance backend systems with Nest JS, Python FastAPI, and Rust",
   avatarUrl: "/me.png",
   skills: [
+    "Flutter",
+    "Dart",
     "React",
     "Next.js",
     "Typescript",
@@ -22,8 +24,7 @@ export const DATA = {
     "Postgres",
     "Docker",
     "Kubernetes",
-    "Java",
-    "C++",
+    "Java"
   ],
   navbar: [
     { href: "/", icon: HomeIcon, label: "Home" },
@@ -35,31 +36,31 @@ export const DATA = {
     social: {
       GitHub: {
         name: "GitHub",
-        url: "https://dub.sh/dillion-github",
+        url: "https://github.com/swarupbhc",
         icon: Icons.github,
 
         navbar: true,
       },
       LinkedIn: {
         name: "LinkedIn",
-        url: "https://dub.sh/dillion-linkedin",
+        url: "https://www.linkedin.com/in/swarupbhc/",
         icon: Icons.linkedin,
 
         navbar: true,
       },
       X: {
         name: "X",
-        url: "https://dub.sh/dillion-twitter",
+        url: "https://x.com/swarupbhc",
         icon: Icons.x,
 
         navbar: true,
       },
-      Youtube: {
-        name: "Youtube",
-        url: "https://dub.sh/dillion-youtube",
-        icon: Icons.youtube,
-        navbar: true,
-      },
+      // Youtube: {
+      //   name: "Youtube",
+      //   url: "https://dub.sh/dillion-youtube",
+      //   icon: Icons.youtube,
+      //   navbar: true,
+      // },
       email: {
         name: "Send Email",
         url: "#",
@@ -72,7 +73,7 @@ export const DATA = {
 
   work: [
     {
-      company: "Atomic Finance",
+      company: "TecTes Technologies Private Limited",
       href: "https://atomic.finance",
       badges: [],
       location: "Remote",
